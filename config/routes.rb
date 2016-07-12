3aa0b264a47bcfda892eef1cc22c9a94f1f26f39
Stephen Akuffo <slyven@gmail.com>
2016-07-12 04:20:37 +0000

    enrollments model

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#index'
 
-  resources :courses, only: [:index, :show]
-  resources :lessons, only: [:show]
+  resources :courses, only: [:index, :show] do
+    resources :enrollments, only: :create
+  end
 
+
+  resources :lessons, only: [:show]
   namespace :instructor do
     resources :sections, only: [] do
       resources :lessons, only: [:new, :create]
